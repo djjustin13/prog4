760f753a28595f122ee7f110538d88e969d76c76
Justin de Ruiter <djjustin13@gmail.com>
2018-04-24 16:16:27 +0200

    added script to generate multiple fish and bubbles

--- week1/js/main.js
+++ week1/js/main.js
@@ -1,24 +1,51 @@
-//
-// geef de vis een random positie en een random kleur
-//
-let fishes = document.getElementsByTagName("fish")
-let fish = fishes[0]
-fish.style.left = "200px"
-fish.style.top = "100px"
-fish.style.webkitFilter = "hue-rotate(200deg)"
-fish.style.filter = "hue-rotate(200deg)"
-
-//
-// geef de bubble een random positie
-//
-let bubbles = document.getElementsByTagName("bubble")
-let bubble = bubbles[0]
-bubble.style.left = "200px"
-bubble.style.top = "0px"
-
-//
-// roep een functie aan als alles geladen is
-//
 window.addEventListener("load", function () {
     console.log("start het aquarium")
+
+    addFish()
+    addBubbles()
 })
+
+function addFish(){
+    let fish = document.createElement("fish")
+    document.body.appendChild(fish)
+    let randomWidth = randomNumber(0, window.innerWidth-130)
+    let randomHeight = randomNumber(0, window.innerHeight-130)
+
+    fish.style.left = randomWidth + "px"
+    fish.style.top = randomHeight + "px"
+    fish.style.webkitFilter = "hue-rotate("+ randomNumber(0,360) +"deg)"
+    fish.style.filter = "hue-rotate("+ randomNumber(0,360) +"deg)"
+
+    fish.addEventListener("click", clickFish)
+
+    if(countElements("fish")< 98){
+        setTimeout(addFish, 100)
+    }
+    
+}
+
+function addBubbles(){
+    let bubble = document.createElement("bubble");
+    document.body.appendChild(bubble);
+    bubble.style.left = randomNumber(0, window.innerWidth-55)+"px"
+    bubble.style.top = randomNumber(0, window.innerHeight-55)+"px"
+
+    if(countElements("bubble")< 98){
+        setTimeout(addBubbles, 100)
+    }
+}
+
+function clickFish(event){
+    let fish = event.target
+    console.log(fish)
+    fish.classList.add("dead")
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function countElements(elmnt){
+    let objects = document.getElementsByTagName(elmnt);
+    return objects.length
+}
